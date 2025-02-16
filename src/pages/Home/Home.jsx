31cbb1d4aead6fd899f1397621c8ac1378966a9e
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-02-16 20:10:42 +0600

    Installed Tailwind v-4 and DaisyUI Beta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import React from "react"
 const Home = () => {
   return (
     <div>
-      <h1>This is Home page</h1>
+      <h1 className="text-red-700">This is Home page</h1>
+      <button class="btn btn-primary">Primary</button>
     </div>
   )
 };

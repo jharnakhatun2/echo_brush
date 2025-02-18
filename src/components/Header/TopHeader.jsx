7814d59c240f5d5aa828c46571ed78455f05ab46
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-02-18 23:11:02 +0600

    Update topHeader style

--- src/components/Header/TopHeader.jsx
+++ src/components/Header/TopHeader.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const TopHeader = () => {
   return (
-    <div className="bg-zinc-900 text-neutral-300 text-center py-1 text-sm">
+    <div className="bg-zinc-800 text-neutral-300 text-center py-1 text-sm">
       <p><strong className="text-green-600">Free shipping</strong> orders from all item</p>
     </div>
   )
